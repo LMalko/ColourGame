2b2842bbc012deae6be48393252b8c75a153c522
LMalko <lmalko2601@gmail.com>
2018-03-08 00:13:03 +0100

    Define getRandomColour() in ColourGame.js

--- ColourGame.js
+++ ColourGame.js
@@ -42,3 +42,8 @@ function generateRandomColours(coloursNumber){
 	return colours
 }
 
+function getRandomColour(){
+	return "rgb(" + Math.floor(Math.random() * 256) + ", " +
+				    Math.floor(Math.random() * 256) + ", " +
+				    Math.floor(Math.random() * 256) + ")"
+}
