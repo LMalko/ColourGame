d0416dfb4adae462b48858b5117bb05365725bf3
LMalko <lmalko2601@gmail.com>
2018-03-20 14:20:51 +0100

    Convert to on() & add to-do in ToDoList.js

--- 2. ToDo List (HTML5, CSS, JS, jQuery 3.3.1)/assets/js/ToDoList.js
+++ 2. ToDo List (HTML5, CSS, JS, jQuery 3.3.1)/assets/js/ToDoList.js
@@ -1,8 +1,8 @@
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-$("span").on("click", function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().css("color", "crimson");
 	$(this).parent().fadeOut(1200, function(){
 		$(this).remove();
@@ -11,5 +11,18 @@ $("span").on("click", function(event){
 	event.stopPropagation();
 
 
+});
+
+$("input[type='text']").on("keypress", function(event){
+ 	if(event.which === 13){
+ 		var toDoText = $(this).val();
+
+ 		// Clear input.
+ 		$(this).val("")
+
+ 		$("ul").append("<li><span>X</span> " + toDoText + "</li>");
+
+
+ 	}
 });
  
