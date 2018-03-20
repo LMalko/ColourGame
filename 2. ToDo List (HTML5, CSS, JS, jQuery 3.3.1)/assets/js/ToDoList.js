9ead700176dec8f4bb1c5a83341d5210bac738d9
LMalko <lmalko2601@gmail.com>
2018-03-20 12:51:58 +0100

    Add span X clicked to ToDoList.css

--- 2. ToDo List (HTML5, CSS, JS, jQuery 3.3.1)/assets/js/ToDoList.js
+++ 2. ToDo List (HTML5, CSS, JS, jQuery 3.3.1)/assets/js/ToDoList.js
@@ -1,3 +1,15 @@
 $("li").on("click", function(){
 	$(this).toggleClass("completed");
 });
+
+$("span").on("click", function(event){
+	$(this).parent().css("color", "crimson");
+	$(this).parent().fadeOut(1200, function(){
+		$(this).remove();
+	});
+	
+	event.stopPropagation();
+
+
+});
+ 
