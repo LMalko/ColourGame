8655b7ef0c1965526f9228e6b4bee2b3312a6e64
LMalko <lmalko2601@gmail.com>
2018-04-11 23:25:50 +0200

    Add campgrounds.ejs to app.js

--- 5 YelpCamp Webpage/app.js
+++ 5 YelpCamp Webpage/app.js
@@ -13,7 +13,7 @@ app.get("/campgrounds", function(req, res){
         {name: "Granite Hill", image: "https://ap.rdcpix.com/1875831382/a087fe9d2ecb81de2d08a21592e53b43l-m0xd-w640_h480_q80.jpg"},
         {name: "Mountain Goat's Rest", image: "https://www.visittheusa.com/sites/default/files/styles/hero_m_1300x700/public/images/hero_media_image/2016-10/HERO%201_Central%20Montana_GettyImages-116363120_CROP_Web72DPI.jpg?itok=UVmCDwUc"}
     ];
-    res.render("campgrounds");
+    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 
