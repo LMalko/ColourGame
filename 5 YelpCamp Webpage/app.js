6743c7ae669fe2dc61c65b516206c61e768d5789
LMalko <lmalko2601@gmail.com>
2018-04-11 23:17:38 +0200

    Add campgrounds.ejs to app.js

--- 5 YelpCamp Webpage/app.js
+++ 5 YelpCamp Webpage/app.js
@@ -7,6 +7,15 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+        {name: "Salmon's Creek", image: ""},
+        {name: "Granite Hill", image: ""},
+        {name: "Mountain Goat's Rest", image: ""}
+    ];
+    res.render("campgrounds");
+});
+
 
 
 app.listen(8080, function(){
