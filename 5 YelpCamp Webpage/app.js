9b65a7a4b81dcd00ba2e9b7aca2fd6f839ada619
LMalko <lmalko2601@gmail.com>
2018-04-12 13:51:54 +0200

    Add post route

--- 5 YelpCamp Webpage/app.js
+++ 5 YelpCamp Webpage/app.js
@@ -5,16 +5,17 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+var campgrounds = [
+    {name: "Salmon's Creek", image: "1.jpg"},
+    {name: "Granite Hill", image: "2.jpg"},
+    {name: "Mountain Goat's Rest", image: "3.jpg"}
+];
+
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Salmon's Creek", image: "1.jpg"},
-        {name: "Granite Hill", image: "2.jpeg"},
-        {name: "Mountain Goat's Rest", image: "3.jpeg"}
-    ];
     res.render("campgrounds", {campgrounds: campgrounds});
 });
 
@@ -23,8 +24,13 @@ app.get("/campgrounds/new", function(req, res){
 });
 
 app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
 
-}
+});
 
 
 app.listen(8080, function(){
