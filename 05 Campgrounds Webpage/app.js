0e7033e1f5e1b1c1a181dc99e0434df59c72f034
LMalko <lmalko2601@gmail.com>
2018-04-19 00:25:15 +0200

    Add mongoDB to app.js

--- 05 Campgrounds Webpage/app.js
+++ 05 Campgrounds Webpage/app.js
@@ -10,18 +10,26 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var campgrounds = [
-    {name: "Salmon's Creek", image: "1.jpg"},
-    {name: "Granite Hill", image: "2.jpg"},
-    {name: "Mountain Goat's Rest", image: "3.jpg"}
-];
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campgrounds = mongoose.model("Campground", campgroundSchema);
+
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campgrounds.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    })
 });
 
 app.get("/campgrounds/new", function(req, res){
@@ -32,9 +40,13 @@ app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-
+    Campgrounds.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 
