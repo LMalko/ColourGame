5a7544e5f981386bfe95342c3440c3b0ea563b30
LMalko <lmalko2601@gmail.com>
2018-04-19 03:55:19 +0200

    Implement show.ejs

--- 05 Campgrounds Webpage/app.js
+++ 05 Campgrounds Webpage/app.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require("body-parser");
 
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:/yelp_camp");
+mongoose.connect("mongodb://localhost:/campgroundsDB");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -16,7 +16,7 @@ var campgroundSchema = new mongoose.Schema({
     description: String
 });
 
-var Campgrounds = mongoose.model("Campground", campgroundSchema);
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 
 app.get("/", function(req, res){
@@ -24,7 +24,7 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-    Campgrounds.find({}, function(err, allCampgrounds){
+    Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err);
         } else {
@@ -40,8 +40,9 @@ app.get("/campgrounds/new", function(req, res){
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    Campgrounds.create(newCampground, function(err, newlyCreated){
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description};
+    Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
         } else {
@@ -51,15 +52,14 @@ app.post("/campgrounds", function(req, res){
 });
 
 app.get("/campgrounds/:id", function(req, res){
-    Campgrounds.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id, function(err, foundCampground){
         if(err){
             console.log(err);
         } else{
-            res.render("show", {campgrounds: foundCampground});
+            res.render("show", {campground: foundCampground});
         }
     });
     req.params.id
-
 });
 
 
