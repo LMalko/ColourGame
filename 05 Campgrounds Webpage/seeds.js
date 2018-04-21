ee52c2bfb7dc9f487fa8a050f190cb3e4f293c1c
LMalko <lmalko2601@gmail.com>
2018-04-21 23:11:26 +0200

    Write seedDB() in seed.js

---
+++ 05 Campgrounds Webpage/seeds.js
@@ -0,0 +1,56 @@
+var mongoose = require("mongoose");
+       Campground = require("./models/campground");
+       Comment = require("./models/comment");
+
+var data = [
+    {
+        name: "Sosnowiec",
+        image: "1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Radom",
+        image: "2.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Kielce",
+        image: "3.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+]
+
+function seedDB(){
+    // Remove all.
+    Campground.remove({}, function(err){
+        if(err){
+            console.log(err)
+        }
+        // Add new campgrounds after removal.
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, data){
+                if(err){
+                    console.log(err)
+                }
+            //    Create a comment.
+                Comment.create(
+                    {
+                        text: "This place is great. Great for vacation."
+                        author: "Gheeta Pirelli"
+                    }, function(err, comment){
+                        if(err){
+                            console.log(err)
+                        } else{
+                            campground.comments.push(comment);
+                            campground.save();
+                        }
+                    }
+                );
+            });
+        });
+    });
+
+}
+
+module.exports = seedDB
+
