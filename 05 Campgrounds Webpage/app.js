daf62d00773d493ac1543f7566afa61b28ae719e
LMalko <lmalko2601@gmail.com>
2018-04-22 14:22:41 +0200

    Add post route for comments in app.js

--- 05 Campgrounds Webpage/app.js
+++ 05 Campgrounds Webpage/app.js
@@ -3,6 +3,7 @@ const express = require("express");
           bodyParser = require("body-parser");
           mongoose = require("mongoose");
           Campground = require("./models/campground");
+          Comment = require("./models/comment");
           seedDB = require("./seeds");
 
 // Clear database.
@@ -38,7 +39,7 @@ app.post("/campgrounds", function(req, res){
     var image = req.body.image;
     var description = req.body.description;
     var newCampground = {name: name, image: image, description: description};
-    Campground.create(newCampground, function(err, newlyCreated){
+    Campground.create(newCampground, function(err){
         if(err){
             console.log(err);
         } else {
@@ -67,6 +68,26 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
             console.log(err);
         } else {
             res.render("comments/new", {campground: campground})
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds")
+        } else {
+            // Use dictionary comment[] from input name in new.ejs.
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + req.params.id);
+                }
+            });
         }
     });
 });
