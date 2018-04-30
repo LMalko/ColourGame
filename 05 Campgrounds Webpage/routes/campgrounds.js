ed55562d481175175e15aee15a5c1887ff2828d2
LMalko <lmalko2601@gmail.com>
2018-05-01 01:20:51 +0200

    Clean code campgrounds.js

--- 05 Campgrounds Webpage/routes/campgrounds.js
+++ 05 Campgrounds Webpage/routes/campgrounds.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Campground = require("../models/campground");
 
 
-router.get("/campgrounds", function(req, res){
+router.get("/", function(req, res){
     Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err);
@@ -13,11 +13,11 @@ router.get("/campgrounds", function(req, res){
     })
 });
 
-router.get("/campgrounds/new", function(req, res){
+router.get("/new", function(req, res){
     res.render("campgrounds/new")
 });
 
-router.post("/campgrounds", function(req, res){
+router.post("/", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
@@ -31,7 +31,7 @@ router.post("/campgrounds", function(req, res){
     });
 });
 
-router.get("/campgrounds/:id", function(req, res){
+router.get("/:id", function(req, res){
     Campground.findById(req.params.id).populate("comments").exec(
         function(err, foundCampground){
             if(err){
