8f5a1ecec2196c3434fef56f56b53fa35056e732
LMalko <lmalko2601@gmail.com>
2018-05-02 00:46:55 +0200

    Assign campground to each user

--- 05 Campgrounds Webpage/routes/campgrounds.js
+++ 05 Campgrounds Webpage/routes/campgrounds.js
@@ -4,7 +4,7 @@ var Campground = require("../models/campground");
 
 // Show all campgrounds.
 
-router.get("/", isLoggedIn, function(req, res){
+router.get("/", function(req, res){
     Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err);
@@ -19,12 +19,20 @@ router.get("/new", isLoggedIn, function(req, res){
     res.render("campgrounds/new")
 });
 
-// Add new campground to DB.
-router.post("/", function(req, res){
+// CREATE - Add new campground to DB.
+router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name: name, image: image, description: description};
+
+    // Assign current user to this new campground.
+    var thisAuthor = {
+        id: req.user._id,
+        username: req.user.username
+    };
+
+    var newCampground = {name: name, image: image,
+        description: description, author: thisAuthor};
     Campground.create(newCampground, function(err){
         if(err){
             console.log(err);
--- 05 Campgrounds Webpage/seeds.js
+++ 05 Campgrounds Webpage/seeds.js
@@ -6,16 +6,19 @@ var data = [
     {
         name: "Sosnowiec",
         image: "1.jpg",
+        author: {username: "Janusz Dzięcioł"},
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     },
     {
         name: "Radom",
         image: "2.jpg",
+        author: {username: "Janusz Dzięcioł"},
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     },
     {
         name: "Kielce",
         image: "3.jpg",
+        author: {username: "Janusz Dzięcioł"},
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
     }
 ];
@@ -28,6 +31,7 @@ function seedDB(){
         }
         // Add new campgrounds after removal.
         data.forEach(function(seed){
+
             Campground.create(seed, function(err, campground){
                 if(err){
                     console.log(err)
