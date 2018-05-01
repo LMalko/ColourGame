dcfbfe74505f9f19a43995acbb09d730bb28be41
LMalko <lmalko2601@gmail.com>
2018-05-02 00:06:40 +0200

    Implemet user authentication before adding campground

--- 05 Campgrounds Webpage/routes/campgrounds.js
+++ 05 Campgrounds Webpage/routes/campgrounds.js
@@ -2,8 +2,9 @@ var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
 
+// Show all campgrounds.
 
-router.get("/", function(req, res){
+router.get("/", isLoggedIn, function(req, res){
     Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err);
@@ -13,10 +14,12 @@ router.get("/", function(req, res){
     })
 });
 
-router.get("/new", function(req, res){
+// NEW - Show form to create new campground.
+router.get("/new", isLoggedIn, function(req, res){
     res.render("campgrounds/new")
 });
 
+// Add new campground to DB.
 router.post("/", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
@@ -31,6 +34,7 @@ router.post("/", function(req, res){
     });
 });
 
+// SHOW - shows more info about one campground.
 router.get("/:id", function(req, res){
     Campground.findById(req.params.id).populate("comments").exec(
         function(err, foundCampground){
@@ -42,6 +46,14 @@ router.get("/:id", function(req, res){
         });
     req.params.id
 });
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl; //Store users current session
+    res.redirect("/login");
+}
 
 
 module.exports = router;
