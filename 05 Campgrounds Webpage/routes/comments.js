aabce46596ce50d20662dff1e2abfacafb4a673f
LMalko <lmalko2601@gmail.com>
2018-05-05 14:23:43 +0200

    Pass campground to comments.js

--- 05 Campgrounds Webpage/routes/comments.js
+++ 05 Campgrounds Webpage/routes/comments.js
@@ -50,16 +50,27 @@ router.post("/",
         });
     });
 
+// Edit route.
 router.get("/:comment_id/edit", function(req, res){
-    Comment.findById(req.params.comment_id, function(err, foundComment){
+    Campground.findById(req.params.id, function(err, campground){
         if(err){
-            res.redirect("back");
+            console.log(err);
+            res.redirect("/campgrounds")
         } else {
-            res.render("comments/edit",
-                {campground_id: req.params.id, comment:foundComment});
+            Comment.findById(req.params.comment_id, function (err, foundComment) {
+                if (err) {
+                    res.redirect("back");
+                } else {
+                    res.render("comments/edit",
+                        {campground_id: req.params.id, comment: foundComment, campground: campground});
+                }
+            });
         }
     });
 });
+
+// Update route.
+
 
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
