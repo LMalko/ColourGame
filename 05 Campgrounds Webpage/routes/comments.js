70b0608f539b4c320b1bf86c78859ee423fa91d9
LMalko <lmalko2601@gmail.com>
2018-05-05 14:36:25 +0200

    Add UPDATE for comments.js

--- 05 Campgrounds Webpage/routes/comments.js
+++ 05 Campgrounds Webpage/routes/comments.js
@@ -71,6 +71,19 @@ router.get("/:comment_id/edit", function(req, res){
 
 // Update route.
 
+router.put("/:comment_id", function(req, res){
+    // comment is an entire comment[text] object because it has only one attribute here - text
+    Comment.findByIdAndUpdate(req.params.comment_id,
+        req.body.comment,
+        function(err, updatedComment){
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
