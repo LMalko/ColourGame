3b6417ab8d49953a13cffca9ce86e19ce98e687e
LMalko <lmalko2601@gmail.com>
2018-05-09 20:19:38 +0200

    Add chackCampgroundOwnership to index.js

--- 05 Campgrounds Webpage/middleware/index.js
+++ 05 Campgrounds Webpage/middleware/index.js
@@ -1,7 +1,28 @@
 
+var middlewareObject = {
 
+};
 
-
+middlewareObject.checkCampgroundOwnership = function(req, res, next){
+        // Check if user is logged in.
+        if(req.isAuthenticated()){
+            Campground.findById(req.params.id, function(err, foundCampground){
+                if(err){
+                    res.redirect("back");
+                } else{
+                    // Does user own campground.
+                    if(foundCampground.author.id.equals(req.user._id)){
+                        next();
+                    } else{
+                        res.redirect("back");
+                    }
+                }
+            });
+            //  If not - redirect.
+        } else {
+            res.redirect("back");
+        }
+    };
 
 
 
