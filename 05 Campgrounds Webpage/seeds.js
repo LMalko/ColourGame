b4ad4110df4d35165b72f42e296e0fceca968069
LMalko <lmalko2601@gmail.com>
2018-05-12 14:04:30 +0200

    Change author in seeds

--- 05 Campgrounds Webpage/seeds.js
+++ 05 Campgrounds Webpage/seeds.js
@@ -9,7 +9,7 @@
 //         price: 22.50,
 //         author: {
 //             username: "admin",
-//             id: "5ae4fa1736273b2468e3c92f"
+//             id: "5af6d7bc44edfe29d0d9e31f"
 //         },
 //         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 //     },
@@ -19,7 +19,7 @@
 //         price: 18.70,
 //         author: {
 //             username: "admin",
-//             id: "5ae4fa1736273b2468e3c92f"
+//             id: "5af6d7bc44edfe29d0d9e31f"
 //         },
 //         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 //     },
@@ -29,7 +29,7 @@
 //         price: 11.67,
 //         author: {
 //             username: "admin",
-//             id: "5ae4fa1736273b2468e3c92f"
+//             id: "5af6d7bc44edfe29d0d9e31f"
 //         },
 //         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
 //     }
