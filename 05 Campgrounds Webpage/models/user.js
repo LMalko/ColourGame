02fb980b5629a31b94765ec6fd5ea5bf7f566074
LMalko <lmalko2601@gmail.com>
2018-05-17 14:00:41 +0200

    Add isAdmin to user model

--- 05 Campgrounds Webpage/models/user.js
+++ 05 Campgrounds Webpage/models/user.js
@@ -4,6 +4,7 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = new mongoose.Schema({
     name: String,
     password: String,
+    isAdmin: {type: Boolean, default: false}
 });
 
 userSchema.plugin(passportLocalMongoose);
