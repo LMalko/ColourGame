020b36d467cb93c1be7da7fd34941ad658d27289
LMalko <lmalko2601@gmail.com>
2018-05-18 03:16:24 +0200

    AAdd moment to install.sh

--- 05 Campgrounds Webpage/install.sh
+++ 05 Campgrounds Webpage/install.sh
@@ -1,2 +1,2 @@
 npm init
-npm install --save express ejs body-parser mongoose passport passport-local passport-local-mongoose express-session method-override connect-flash dotenv node-geocoder
+npm install --save express ejs body-parser mongoose passport passport-local passport-local-mongoose express-session method-override connect-flash dotenv node-geocoder moment
