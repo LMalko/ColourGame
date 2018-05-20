fd30b82d03496f109db5d17f40e2c12b4649aa2a
LMalko <lmalko2601@gmail.com>
2018-05-21 00:44:54 +0200

    Add attributes to user model in user.js

--- 05 Campgrounds Webpage/models/user.js
+++ 05 Campgrounds Webpage/models/user.js
@@ -4,7 +4,11 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = new mongoose.Schema({
     name: String,
     password: String,
-    isAdmin: {type: Boolean, default: false}
+    isAdmin: {type: Boolean, default: false},
+    avatar: String,
+    firstName: String,
+    lastName: String,
+    email: String
 });
 
 userSchema.plugin(passportLocalMongoose);
