d068e0920543faae4324ec290f5d1e822f976de6
LMalko <lmalko2601@gmail.com>
2018-05-26 23:39:03 +0200

    Add GET forgot in index.js

--- 05 Campgrounds Webpage/routes/index.js
+++ 05 Campgrounds Webpage/routes/index.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var router = express.Router();
 
+var passport = require("passport");
+var User = require("../models/user");
+var Campground = require("../models/campground");
+var async = require("async");
+var nodemailer = require("nodemailer");
+var crypto = require("crypto");
+
 router.get("/", function(req, res){
     res.render("landing");
 });
+
+// forgot password
+router.get('/forgot', function(req, res) {
+    res.render('forgot');
+});
+
+
 
 module.exports = router;
