6ef9697d836ba907c8a1a84cc7a42e4a704aefc4
LMalko <lmalko2601@gmail.com>
2018-06-10 17:32:24 +0200

    Disable cache in app.js

--- 05 Campgrounds Webpage/app.js
+++ 05 Campgrounds Webpage/app.js
@@ -33,7 +33,11 @@ app.use(methodOverride("_method"));
 // Flash needs to be before passport configuration.
 app.use(flash());
 
-
+app.use(function(req, res, next) {
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, ' +
+        'max-stale=0, post-check=0, pre-check=0');
+    next();
+});
 
 // Clear database & and populate. Commented out to save new objects permanently.
 
@@ -95,6 +99,8 @@ app.use("/campgrounds", campgroundRoutes);
 // Same for comments, index doesn't have common address.
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use(userRoutes);
+
+
 
 
 
