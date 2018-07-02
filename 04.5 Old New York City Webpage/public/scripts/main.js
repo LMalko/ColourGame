4dcecb038d4c04906430979fa6e5d737e865bddd
LMalko <lmalko2601@gmail.com>
2018-07-02 19:46:52 +0200

    Add change photos functionality in main.js

--- 04.5 Old New York City Webpage/public/scripts/main.js
+++ 04.5 Old New York City Webpage/public/scripts/main.js
@@ -2,3 +2,24 @@ $('.ui .item').on('click', function() {
     $('.ui .item').removeClass('active');
     $(this).addClass('active');
 });
+
+var images = ["https://res.cloudinary.com/campgrounds2018/image/upload/v1530552630/1.jpg",
+                            "https://res.cloudinary.com/campgrounds2018/image/upload/v1530552630/2.jpg",
+                            "https://res.cloudinary.com/campgrounds2018/image/upload/v1530552630/3.jpg",
+                            "https://res.cloudinary.com/campgrounds2018/image/upload/v1530552630/4.jpg",
+                            "https://res.cloudinary.com/campgrounds2018/image/upload/v1530552630/5.jpg",
+                            "https://res.cloudinary.com/campgrounds2018/image/upload/v1530552630/6.jpg",
+                            "https://res.cloudinary.com/campgrounds2018/image/upload/v1530552630/7.jpg",
+                            "https://res.cloudinary.com/campgrounds2018/image/upload/v1530552631/8.jpg"];
+function previousImage(){
+    var indexPosition = images.indexOf(document.getElementById("IndexImage").src);
+    if (indexPosition > 0) {
+        document.getElementById("IndexImage").src = images[indexPosition - 1];
+    }
+}
+function nextImage(){
+    var indexPosition = images.indexOf(document.getElementById("IndexImage").src);
+    if (indexPosition < 7) {
+        document.getElementById("IndexImage").src = images[indexPosition + 1];
+    }
+}
